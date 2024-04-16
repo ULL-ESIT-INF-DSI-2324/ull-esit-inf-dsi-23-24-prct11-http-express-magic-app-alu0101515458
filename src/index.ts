72b848cafbcbb019c0a20b8a6a601822735f3f98
Tomás Javes <alu0101515458@ull.edu.es>
2024-04-16 23:57:59 +0000

    Añadiendo configuraciones iniciales del repositorio

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function add(firstNum: number, secondNum: number): number {
+  return firstNum + secondNum;
+}
+
+const myConst: number = 7;
+let myResult: number = add(1, myConst);
+console.log(`myResult = ${myResult}`);
